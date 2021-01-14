8e6e1e459ba28c405da10a9bdc83a3ab0fa81a9d
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2021-01-15 01:50:31 +0530

    Added test project

---
+++ _projects/test_project.md
@@ -0,0 +1,8 @@
+---
+title: "Test Project"
+excerpt: "Tes Project subtitle"
+
+---
+
+Test_content
+Try this https://raw.githubusercontent.com/mmistakes/minimal-mistakes/master/docs/_portfolio/foo-bar-website.md
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Test Project",
+        "excerpt":"Test_content Try this https://raw.githubusercontent.com/mmistakes/minimal-mistakes/master/docs/_portfolio/foo-bar-website.md  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/projects/test_project/",
+        "teaser": "http://localhost:4000/assets/images/bog_end_sea.jpg"
+      }]
