5945d92929814912fedcaa5bb0fe6dcd1b686f0d
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2021-01-15 02:21:27 +0530

    Added modbot prokect skeleton

--- _projects/test_project.md
+++ _projects/test_project.md
@@ -1,8 +1,24 @@
 ---
-title: "Test Project"
-excerpt: "Tes Project subtitle"
-
+title: "Modular Robots"
+excerpt: "Description"
+header:
+  teaser: /assets/images/projects/modbot_dp.jpeg
+gallery:
+  - url: /assets/images/projects/modbot1.jpg
+    image_path: /assets/images/projects/modbot1.jpg
+    alt: "placeholder image 1"
+  - url: /assets/images/projects/modbot2.png
+    image_path: /assets/images/projects/modbot2.png
+    alt: "placeholder image 2"
+  - url: /assets/images/projects/modbot_dp.jpeg
+    image_path: /assets/images/projects/modbot_dp.jpeg
+    alt: "placeholder image 3"
 ---
 
 Test_content
 Try this https://raw.githubusercontent.com/mmistakes/minimal-mistakes/master/docs/_portfolio/foo-bar-website.md
+
+
+{% include gallery caption="This is a sample gallery to go along with this case study." %}
+
+Content after gallery...
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Test Project",
-        "excerpt":"Test_content Try this https://raw.githubusercontent.com/mmistakes/minimal-mistakes/master/docs/_portfolio/foo-bar-website.md  ","categories": [],
+        "title": "Modular Robots",
+        "excerpt":"Test_content Try this https://raw.githubusercontent.com/mmistakes/minimal-mistakes/master/docs/_portfolio/foo-bar-website.md                                                                                                                                This is a sample gallery to go along with this case study.       Content after gallery…  ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/projects/test_project/",
-        "teaser": "http://localhost:4000/assets/images/bog_end_sea.jpg"
+        "teaser": "http://localhost:4000/assets/images/projects/modbot_dp.jpeg"
       }]
