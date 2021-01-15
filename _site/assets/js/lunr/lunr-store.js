71728105c3dfdb20e0fdce72a9232b5050d2ced3
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2021-01-15 17:32:50 +0530

    Updated to jekyll 4.2 and reordered projects

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,18 +2,18 @@ var store = [{
         "title": "Modular Robots",
         "excerpt":"Modular Robots are a system of individual modules that are capable of physically linking with each other to form structures suited for different purposes. These structures are able to reconfigure themselves into different configurations, each suited for a different objective. This ability provides versatility to the robots. Whenever the bot...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/projects/modbot/",
-        "teaser": "http://localhost:4000/assets/images/projects/modbot_dp.jpeg"
-      },{
-        "title": "Swarmbots",
-        "excerpt":"Swarmbots is a project on development of algorithms for automation of a swarm of autonomous mobile robots. The robots perform various tasks through ‘collective decision making’. Multi-robot systems become increasingly complex with increase in the numbre of robots. In this project, the system is entirely decentralized. Each robot knows only...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/projects/swarmbots/",
-        "teaser": "http://localhost:4000/assets/images/projects/swarmbots_dp.jpeg"
+        "url": "/projects/modbot/",
+        "teaser": "/assets/images/projects/modbot_dp.jpeg"
       },{
         "title": "Trotbot",
         "excerpt":"Trotbot is an autonomous omnidirectional ground robot, designed to serve as an indoor delivery robot. It is able to autonomously navigate around dynamic obstacles and find its way to a given waypoint. The robot uses a 2D LIDAR for obstacle detection and a RealSense sensor for visual-inertial localization. The planner...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/projects/trotbot/",
-        "teaser": "http://localhost:4000/assets/images/projects/trotbot_dp.jpeg"
+        "url": "/projects/trotbot/",
+        "teaser": "/assets/images/projects/trotbot_dp.jpeg"
+      },{
+        "title": "Swarmbots",
+        "excerpt":"Swarmbots is a project on development of algorithms for automation of a swarm of autonomous mobile robots. The robots perform various tasks through ‘collective decision making’. Multi-robot systems become increasingly complex with increase in the numbre of robots. In this project, the system is entirely decentralized. Each robot knows only...","categories": [],
+        "tags": [],
+        "url": "/projects/swarmbots/",
+        "teaser": "/assets/images/projects/swarmbots_dp.jpeg"
       }]
