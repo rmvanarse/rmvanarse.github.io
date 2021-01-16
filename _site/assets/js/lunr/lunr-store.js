15c4cfd06eaed89ddb42a3f248958a5bea64b8d2
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2021-01-16 19:50:00 +0530

    Added empty teaching tab

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,48 +2,48 @@ var store = [{
         "title": "Modular Robots",
         "excerpt":"Modular Robots are a system of individual modules that are capable of physically linking with each other to form structures suited for different purposes. These structures are able to reconfigure themselves into different configurations, each suited for a different objective. This ability provides versatility to the robots. Whenever the bot...","categories": [],
         "tags": [],
-        "url": "/projects/modbot/",
+        "url": "/projects/s01_modbot/",
         "teaser": "/assets/images/projects/modbot_dp.jpeg"
       },{
         "title": "Trotbot",
         "excerpt":"Trotbot is an autonomous omnidirectional ground robot, designed to serve as an indoor delivery robot. It is able to autonomously navigate around dynamic obstacles and find its way to a given waypoint. The robot uses a 2D LIDAR for obstacle detection and a RealSense sensor for visual-inertial localization. The planner...","categories": [],
         "tags": [],
-        "url": "/projects/trotbot/",
+        "url": "/projects/s02_trotbot/",
         "teaser": "/assets/images/projects/trotbot_dp.jpeg"
       },{
         "title": "Swarmbots",
         "excerpt":"Swarmbots is a project on development of algorithms for automation of a swarm of autonomous mobile robots. The robots perform various tasks through ‘collective decision making’. Multi-robot systems become increasingly complex with increase in the numbre of robots. In this project, the system is entirely decentralized. Each robot knows only...","categories": [],
         "tags": [],
-        "url": "/projects/swarmbots/",
+        "url": "/projects/s03_swarmbots/",
         "teaser": "/assets/images/projects/swarmbots_dp.jpeg"
       },{
         "title": "Visual-Inertial SLAM for AUVs",
         "excerpt":"Underwater visual odometry poses several challenges due to attenuation of light, reflections, suspended particles, poor features, absence of GPS, inability to use LIDARs, etc. A light-transmission based image enhancement pipeline was developed in this project to eleminate degradations in underwater visual data and improve the performance of SLAM &amp; VIO...","categories": [],
         "tags": [],
-        "url": "/projects/thesis/",
+        "url": "/projects/s04_thesis/",
         "teaser": "/assets/images/projects/thesis_dp.png"
       },{
         "title": "Robot Localization using Object Tracking",
         "excerpt":"This project attempts to model localization in robots as humans do while navigating in the real world. Humans use their semantic understanding of the environment to often track entire ‘objects of interest’ rather than relying only on features/keypoints (as done in most VIO/SLAM methods). The focus of this project was...","categories": [],
         "tags": [],
-        "url": "/projects/object_tracking/",
+        "url": "/projects/s05_object_tracking/",
         "teaser": "/assets/images/projects/cogneuro_dp.png"
       },{
         "title": "Multispectral Image Registration",
         "excerpt":"Precision agriculture often require images from different spectra in infrared &amp; visual for gaining nutritional information about the crop. In this project, multispectral images of a field were taken by flying a drone over the field. A method for alignment of images from different spectra was developed. Infrared images differ...","categories": [],
         "tags": [],
-        "url": "/projects/multispectral/",
+        "url": "/projects/s06_multispectral/",
         "teaser": "/assets/images/projects/farm.png"
       },{
         "title": "Automatic Signature Verification",
         "excerpt":"Signature verification involves distinguishing between authentic signatures and forgeries. Inspired from facial recognition networks, a one-shot learning convolutional neural network was designed to detect forgeries of a given signature. A Siamese architecture was developed using PyTorch and was trained using triplet loss. A dataset containing several authentic signatures &amp; forged...","categories": [],
         "tags": [],
-        "url": "/projects/ps1/",
+        "url": "/projects/s07_ps1/",
         "teaser": "/assets/images/projects/ps1_dp.png"
       },{
         "title": "LED Globe",
         "excerpt":"The LED Globe was a large semicircular arc (1.5m diameter) lined with RGB LEDs. By rotating at a high speed about a vertical axis, it could display images mid-air, like a hologram. Each image was split into vertical segments, that were sent to the LEDs in a timed manner as...","categories": [],
         "tags": [],
-        "url": "/projects/globe/",
+        "url": "/projects/s08_globe/",
         "teaser": "/assets/images/projects/globe_dp.jpeg"
       }]
