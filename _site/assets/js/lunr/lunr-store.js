6f2a71e6fc88a6ea9739bfb46517b890f4cdb3c5
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2021-12-08 22:45:54 -0500

    Compiled previous commit

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -59,27 +59,27 @@ var store = [{
         "url": "/projects/s04_thesis/",
         "teaser": "/assets/images/projects/thesis_dp.png"
       },{
-        "title": "Robot Localization using Object Tracking",
-        "excerpt":"This project attempts to model localization in robots as humans do while navigating in the real world. Humans use their semantic understanding of the environment to often track entire ‘objects of interest’ rather than relying only on features/keypoints (as done in most VIO/SLAM methods). The focus of this project was...","categories": [],
-        "tags": [],
-        "url": "/projects/s05_object_tracking/",
-        "teaser": "/assets/images/projects/cogneuro_dp.png"
-      },{
         "title": "Multispectral Image Registration",
         "excerpt":"Precision agriculture often require images from different spectra in infrared &amp; visual for gaining nutritional information about the crop. In this project, multispectral images of a field were taken by flying a drone over the field. A method for alignment of images from different spectra was developed. Infrared images differ...","categories": [],
         "tags": [],
         "url": "/projects/s06_multispectral/",
         "teaser": "/assets/images/projects/farm.png"
       },{
-        "title": "Automatic Signature Verification",
-        "excerpt":"Signature verification involves distinguishing between authentic signatures and forgeries. Inspired from facial recognition networks, a one-shot learning convolutional neural network was designed to detect forgeries of a given signature. A Siamese architecture was developed using PyTorch and was trained using triplet loss. A dataset containing several authentic signatures &amp; forged...","categories": [],
+        "title": "Robot Localization using Object Tracking",
+        "excerpt":"This project attempts to model localization in robots as humans do while navigating in the real world. Humans use their semantic understanding of the environment to often track entire ‘objects of interest’ rather than relying only on features/keypoints (as done in most VIO/SLAM methods). The focus of this project was...","categories": [],
         "tags": [],
-        "url": "/projects/s07_ps1/",
-        "teaser": "/assets/images/projects/ps1_dp.png"
+        "url": "/projects/s07_object_tracking/",
+        "teaser": "/assets/images/projects/cogneuro_dp.png"
       },{
         "title": "LED Globe",
         "excerpt":"The LED Globe was a large semicircular arc (1.5m diameter) lined with RGB LEDs. By rotating at a high speed about a vertical axis, it could display images mid-air, like a hologram. Each image was split into vertical segments, that were sent to the LEDs in a timed manner as...","categories": [],
         "tags": [],
         "url": "/projects/s08_globe/",
         "teaser": "/assets/images/projects/globe_dp.jpeg"
+      },{
+        "title": "Automatic Signature Verification",
+        "excerpt":"Signature verification involves distinguishing between authentic signatures and forgeries. Inspired from facial recognition networks, a one-shot learning convolutional neural network was designed to detect forgeries of a given signature. A Siamese architecture was developed using PyTorch and was trained using triplet loss. A dataset containing several authentic signatures &amp; forged...","categories": [],
+        "tags": [],
+        "url": "/projects/s09_ps1/",
+        "teaser": "/assets/images/projects/ps1_dp.png"
       },]
